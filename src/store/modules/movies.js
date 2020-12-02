fbd8d6ac21cad9b80d0fc220868c0be35e865704
triabagus <triatop9@gmail.com>
2020-12-02 13:39:09 +0700

    improvement(Vuex): update movie

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -3,7 +3,8 @@ import movieList from '../../assets/movie-list'
 const SET_SEARCH = "SET_SEARCH"
 const SET_FILTER = "SET_FILTER"
 const ADD_MOVIE  = "ADD_MOVIE" 
-const DELETE_MOVIE = 'DELETE_MOVIE'
+const DELETE_MOVIE = "DELETE_MOVIE"
+const UPDATE_MOVIE = "UPDATE_MOVIE"
 
 const state = {
     movies: movieList,
@@ -29,6 +30,16 @@ const mutations = {
 
     [DELETE_MOVIE](state, id){
         state.movies.splice(state.movies.findIndex(movie => movie.id === id), 1)
+    },
+
+    [UPDATE_MOVIE](state, movie){
+        state.movies = state.movies.map(oldMovie => {
+            if(movie.id === oldMovie.id){
+                return movie
+            }
+
+            return oldMovie
+        } ) 
     }
 }
 
@@ -48,6 +59,10 @@ const actions = {
 
     deleteMovie({commit}, id){
         commit(DELETE_MOVIE, id) 
+    },
+
+    updateMovie({commit}, movie){
+        commit(UPDATE_MOVIE, movie)
     }
 }
 

71715abe432e1d9b2080f71c3560ddc683f65a0d
triabagus <triatop9@gmail.com>
2020-12-05 10:48:15 +0700

    improvemenet: scroll top and avatar img

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="navbar-wrap"> 
-        <div id="navbar">
+    <div id="navbar-wrap" :class="{ 'navbar-scroll': !showNavbar }" >  
+        <div id="navbar" :class="{ 'navbar-padding': !showNavbar }">
             <h2 @click="$router.push('/')">Movie.io</h2>
             <div v-if="$route.path == '/'"> 
                 <input v-model="search" type="text" placeholder="Maybe Avatar?" /> 
             </div>
-			<h2>Logo</h2>
+			<img :src="'../assets/img/avatar.png'" alt="avatar"> 
         </div>
     </div>
 </template>
@@ -14,7 +14,31 @@
 export default {
     data (){
 		return{
-			search:'',
+			search: '', 
+			showNavbar: true,
+			lastScrollPosition: 0
+		}
+	},
+
+	mounted () {
+		window.addEventListener('scroll', this.onScroll)
+	}, 
+
+	methods: {
+		onScroll () {
+			// Get the current scroll position
+			const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
+			// Because of momentum scrolling on mobiles, we shouldn't continue if it is less than zero
+			if (currentScrollPosition < 0) {
+				return
+			}
+			// current scroll position and last scroll position is less than some offset
+			if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60) {
+				return
+			}
+
+			this.showNavbar = currentScrollPosition < this.lastScrollPosition
+			this.lastScrollPosition = currentScrollPosition
 		}
 	},
 
@@ -27,16 +51,31 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+ 
 	#navbar-wrap {
 		position: fixed;
 		width: 100%;
-		z-index: 9999;
+		z-index: 9999; 
+		transform: translate3d(0, 0, 0);
+		transition: 0.1s all ease-out; 
+
+		&.navbar-scroll {  
+			box-shadow: 0 2px 15px #1d1d1d;
+		}
 
 		#navbar {
 			display: flex;
 			padding: 20px;
 			justify-content: space-between;
 			background-color:#070608;
+
+			&.navbar-padding {
+				padding:25px 20px;
+			}
+
+			img { 
+				width:40px;
+			}
 
 			h2 {
 				margin: 2px 1rem 0 1rem;
